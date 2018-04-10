7f0e2e51b7ce50676ac1154bbe5b0f54c599cd2f
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-04-09 23:13:36 -0500

    main functionality of the game works as intended

--- game.js
+++ game.js
@@ -11,63 +11,26 @@ $(document).ready(function () {
         'oat itain oat tain nate eate tea anne inant nean',
         'itant eate anot eat nato inate eat anot tain eat',
         'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-    //each sentence is a string
-    //can count through each sentence string to match charCodes with user input
-    let sentence1 = sentences[0];
-    let sentence2 = sentences[1];
-    let sentence3 = sentences[2];
-    let sentence4 = sentences[3];
-    let sentence5 = sentences[4];
 
     //counter for which sentence user is on
     let sentIndex = 0;
     //counter for which character user is on in each sentence
     let charIndex = 0;
 
-    //display sentences one at a time
     //creating each sentence as paragraphs to callback to hide or show
     let $firstSentence = $('<p>' + sentences[0] + '</p>');
     let $secondSentence = $('<p>' + sentences[1] + '</p>');
     let $thirdSentence = $('<p>' + sentences[2] + '</p>');
     let $fourthSentence = $('<p>' + sentences[3] + '</p>');
     let $fifthSentence = $('<p>' + sentences[4] + '</p>');
+   
     //appending each sentence to the div with id "sentence"
     $('#sentence').append($firstSentence);
-    $('#sentence').append($secondSentence);
-    $('#sentence').append($thirdSentence);
-    $('#sentence').append($fourthSentence);
-    $('#sentence').append($fifthSentence);
-    //each if statement checks the sentIndex to show the correct sentence the user is on
-    if (sentIndex === 0) {
-        $($secondSentence).hide();
-        $($thirdSentence).hide();
-        $($fourthSentence).hide();
-        $($fifthSentence).hide();
-    }
-    if (sentIndex === 1) {
-        $($firstSentence).hide();
-        $($thirdSentence).hide();
-        $($fourthSentence).hide();
-        $($fifthSentence).hide();
-    }
-    if (sentIndex === 2) {
-        $($firstSentence).hide();
-        $($secondSentence).hide();
-        $($fourthSentence).hide();
-        $($fifthSentence).hide();
-    }
-    if (sentIndex === 3) {
-        $($firstSentence).hide();
-        $($secondSentence).hide();
-        $($thirdSentence).hide();
-        $($fifthSentence).hide();
-    }
-    if (sentIndex === 4) {
-        $($firstSentence).hide();
-        $($secondSentence).hide();
-        $($thirdSentence).hide();
-        $($fourthSentence).hide();
-    }
+    //$('#sentence').append($secondSentence);
+    //$('#sentence').append($thirdSentence);
+    //$('#sentence').append($fourthSentence);
+    //$('#sentence').append($fifthSentence);
+
     //Display currently expected letter
     var expected = getExpectedCharCode(sentIndex, charIndex, sentences);
     $target = String.fromCharCode(expected);
@@ -79,16 +42,30 @@ $(document).ready(function () {
         var expected = getExpectedCharCode(sentIndex, charIndex, sentences);
         //checks if the correct letter was presed
         if (event.which == expected) {
-            
             //display green check into div #feedback
             console.log('correct');
             //animate highlight to next letter
             //add 1 to the charIndex counter
             charIndex += 1
-            console.log(charIndex);
+            //console.log(charIndex);
+            //resets expected to next letter to properly display string on screen
+            var expected = getExpectedCharCode(sentIndex, charIndex, sentences);
             $target = String.fromCharCode(expected);
             $('#target-letter p').replaceWith("<p>" + $target + "</p>");
+            console.log(sentIndex);
         }
+        //when user reaches end of sentence, display next sentence and reset charIndex = 0 for next sentence
+        else if (charIndex == sentences[sentIndex].length) {
+            //go to next sentence
+            sentIndex += 1;
+            console.log('sentIndex = ' + sentIndex);
+            //start at the beginning of the new sentence
+            charIndex = 0;
+            console.log('charIndex = ' + charIndex);
+            //display current sentence the user is on
+            $('#sentence p').replaceWith('<p>' + sentences[sentIndex] + '</p>');
+        }
+
         //incorrect letter pressed
         else if (event.which !== expected) {
             //display red 'x' into div #feedback
@@ -98,7 +75,6 @@ $(document).ready(function () {
             console.log(mistakes);
         }
     });
-
     /* 
     function to return an expected char code of the next letter
     sentence = sentIndex
